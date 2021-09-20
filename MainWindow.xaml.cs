55ffada9a5335ffe2350ae6c89319a0c2794cd25
kukha <kukha@WORKPC>
2021-09-20 22:36:53 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fileUsers = "giveMeYourPassword.txt";
+        bool loged = false;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string log = Login.Text;
+            string pass = Password.Password;
+            repeater:
+            if (File.Exists(fileUsers))
+            {
+                foreach (string str in File.ReadAllLines(fileUsers))
+                {
+                    if (log.Equals(str.Split('/')[0]) && pass.Equals(str.Split('/')[1]))
+                    {
+                        loged = true;
+                        new UserApp(log, loged, 0).Show();
+                        Close();
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                File.WriteAllText(fileUsers, "Admin/asap/full\nUser/sup");
+                goto repeater;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ UserApp.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Логика взаимодействия для UserApp.xaml
+    /// </summary>
+    public partial class UserApp : Window
+    {
+        string user;
+        bool loged;
+        int access;
+
+        public UserApp(string user, bool loged, int access)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.loged = loged;
+            this.access = access;
+            if (!this.loged)
+            {
+                Application.Current.Shutdown();
+            }
+            WindowUser.Title = this.user;
+        }
+    }
+}
